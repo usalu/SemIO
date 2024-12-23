942551376fce5525219d06d69884ab55a141938d
Ueli Saluz <ueli.saluz@iek.uni-hannover.de>
2024-12-23 21:32:38 +0100

    updating icons

--- logo/build.ps1
+++ logo/build.ps1
@@ -9,3 +9,22 @@
 
 & magick emblem_dark_round_1920x1920.png -define icon:auto-resize="256,128,96,64,48,32,16" favicon_dark.ico
 & magick emblem_dark_round_1920x1920.png -define icon:auto-resize="32" favicon_dark_32x32.ico
+
+# resize png to 1920x1920 to 512x512, 256x256, 192x192, 180x180, 152x152, 144x144, 120x120, 114x114, 96x96, 72x72, 60x60, 57x57, 48x48, 36x36, 32x32, 24x24, 16x16
+& magick emblem_1920x1920.png -resize 512x512 emblem_512x512.png
+& magick emblem_1920x1920.png -resize 256x256 emblem_256x256.png
+& magick emblem_1920x1920.png -resize 192x192 emblem_192x192.png
+& magick emblem_1920x1920.png -resize 180x180 emblem_180x180.png
+& magick emblem_1920x1920.png -resize 152x152 emblem_152x152.png
+& magick emblem_1920x1920.png -resize 144x144 emblem_144x144.png
+& magick emblem_1920x1920.png -resize 120x120 emblem_120x120.png
+& magick emblem_1920x1920.png -resize 114x114 emblem_114x114.png
+& magick emblem_1920x1920.png -resize 96x96 emblem_96x96.png
+& magick emblem_1920x1920.png -resize 72x72 emblem_72x72.png
+& magick emblem_1920x1920.png -resize 60x60 emblem_60x60.png
+& magick emblem_1920x1920.png -resize 57x57 emblem_57x57.png
+& magick emblem_1920x1920.png -resize 48x48 emblem_48x48.png
+& magick emblem_1920x1920.png -resize 36x36 emblem_36x36.png
+& magick emblem_1920x1920.png -resize 32x32 emblem_32x32.png
+& magick emblem_1920x1920.png -resize 24x24 emblem_24x24.png
+& magick emblem_1920x1920.png -resize 16x16 emblem_16x16.png
