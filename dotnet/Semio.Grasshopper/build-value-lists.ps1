9684f6e33787bbd116da44b4e78e00b1c93f897a
Ueli Saluz <ueli.saluz@iek.uni-hannover.de>
2025-01-06 16:17:31 +0100

    refactored build

--- dotnet/Semio.Grasshopper/build-value-lists.ps1
+++ dotnet/Semio.Grasshopper/build-value-lists.ps1
@@ -1,38 +1,24 @@
 # Create value list for Grasshopper MIME types and licenses
 
-# Turns:
-# Extension, MIME, Name
-# .glb, GLTF Binary, Graphics Library Transmission Format Binary
-# .gltf, GLTF JSON, Graphics Library Transmission Format JSON
-# into:
-# .glb     = "model/gltf-binary"
-# .gltf    = "model/gltf+json"
-
-# Turns:
-# License, Name
-# MIT, MIT License
-# GPL-3.0, GNU General Public License v3.0
-# into:
-# MIT      = "MIT License"
-# GPL-3.0  = "GNU General Public License v3.0"
-
 $buildDir = ".\build"
 if (-not (Test-Path -Path $buildDir)) {
     New-Item -ItemType Directory -Path $buildDir
 }
 
-# Process MIME types
-$mimesCsv = Import-Csv -Path "..\..\meta\mimes.csv"
-$mimeOutputFilePath = Join-Path -Path $buildDir -ChildPath "mimes.txt"
-$mimesCsv | ForEach-Object {
-    $line = "$($_.Extension) = `"$($_.MIME)`""
-    Write-Output $line
-} | Out-File -FilePath $mimeOutputFilePath
+function Convert-CsvToValueList {
+    param (
+        [string]$csv,
+        [string]$output,
+        [string]$key,
+        [string]$value
+    )
 
-# Process licenses
-$licensesCsv = Import-Csv -Path "..\..\meta\licenses.csv"
-$licensesOutputFilePath = Join-Path -Path $buildDir -ChildPath "licenses.txt"
-$licensesCsv | ForEach-Object {
-    $line = "$($_.Name) = `"$($_.SPDX)`""
-    Write-Output $line
-} | Out-File -FilePath $licensesOutputFilePath
+    $csvData = Import-Csv -Path $csv
+    $csvData | ForEach-Object {
+        $line = "$($_.$key) = `"$($_.$value)`""
+        Write-Output $line
+    } | Out-File -FilePath $output
+}
+
+Convert-CsvToValueList -csv "..\..\meta\mimes.csv" -output (Join-Path -Path $buildDir -ChildPath "mimes.txt") -key "Extension" -value "MIME"
+Convert-CsvToValueList -csv "..\..\meta\licenses.csv" -output (Join-Path -Path $buildDir -ChildPath "licenses.txt") -key "Name" -value "SPDX"
